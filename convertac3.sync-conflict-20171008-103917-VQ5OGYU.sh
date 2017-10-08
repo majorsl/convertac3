6f51310e8fd4afe6bff896413ae171a880c4bd1a
Steve Major <steve@themajorshome.com>
2017-10-08 13:52:12 -0400

    Env bash change, update to tag, now only acc triggers re-encode.

--- convertac3.sync-conflict-20171008-103917-VQ5OGYU.sh
+++ convertac3.sync-conflict-20171008-103917-VQ5OGYU.sh
@@ -1,15 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # Version 0.3.2 *REQUIREMENTS BELOW*
 #
 # 1. Working Homebrew installed.
 # 2. Homebrew: brew tap caskroom/cask
 # 3. Homebrew: brew install ffmpeg
 # 4. Homebrew: brew install terminal-notifier
-# 5. Homebrew: brew install tag
+# 5. Homebrew: brew install osxutils
 # 6. Homebrew: brew install detox
 
-# This script will attempt to convert mkv files with one or more aac tracks to ac3 and will
-# tag those converted files with a finder label of blue.
+# This script will attempt to convert mkv files with one or more aac tracks to ac3.
 
 # SET YOUR OPTIONS HERE -------------------------------------------------------------------------
 # directory to parse files recursively.
@@ -21,30 +20,19 @@ BACKUPDIRECTORY="/Volumes/Drobo/Media Center/Backups/"
 # path to terminal-notifier
 TERMINALNOTIFIER="/usr/local/bin/"
 
-# path to ffmpeg
-FFMPEG="/usr/local/opt/ffmpeg/bin/"
-
-# path to detox
-DETOX="/usr/local/opt/detox/bin/"
-
-#path to tag
-TAG="/usr/local/bin/"
-
 # -----------------------------------------------------------------------------------------------
 IFS=$'\n'
 
-"$DETOX"detox -r -v "$WORKINGDIRECTORY"
-for file in $(find "$WORKINGDIRECTORY" -type f -name "*.mkv")
-do
-#find "$WORKINGDIRECTORY" -type f -name "*.mkv" -print0 | while IFS= read -r -d $'\0' file; do
+detox -r -v "$WORKINGDIRECTORY"
+find "$WORKINGDIRECTORY" -type f -name "*.mkv" -print0 | while IFS= read -r -d $'\0' file; do
 echo $file
-acodec=$("$FFMPEG"ffprobe -v error -select_streams a -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 $file)
+acodec=$(ffprobe -v error -select_streams a -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 $file)
 echo $acodec
 if [ "$acodec" = "aac" ]; then
 	newfile=${file%.*}
 	"$TERMINALNOTIFIER"terminal-notifier -title 'Convert AC3' -message "Processing $file" -activate -timeout 10
-	"$FFMPEG"ffmpeg -i "$file" -vcodec copy -scodec copy -acodec ac3 -ac 6 -ab 448k "$newfile"-AC3-.mkv
-	"$TAG"tag -a Blue "$newfile"-AC3-.mkv
+	ffmpeg -i "$file" -vcodec copy -scodec copy -acodec ac3 -ac 6 -ab 448k "$newfile"-AC3-.mkv
+	/usr/local/bin/setlabel Grey "$newfile"-AC3-.mkv
 	mv $file $BACKUPDIRECTORY
 fi
 done
