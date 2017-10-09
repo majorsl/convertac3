186d30ac3a82ff0f70190c8635b6497721ffb5d5
Steve Major <steve@themajorshome.com>
2017-10-09 12:06:34 -0400

    Double-quotes are your friend to prevent globbing.

--- convertac3.sh
+++ convertac3.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Version 0.3.3 *See README.md for requirements*
+# Version 0.3.4 *See README.md for requirements*
 #
 # SET YOUR OPTIONS HERE -------------------------------------------------------------------------
 # directory to parse files recursively.
@@ -27,15 +27,15 @@ IFS=$'\n'
 for file in $(find "$WORKINGDIRECTORY" -type f -name "*.mkv")
 do
 #find "$WORKINGDIRECTORY" -type f -name "*.mkv" -print0 | while IFS= read -r -d $'\0' file; do
-echo $file
-acodec=$("$FFMPEG"ffprobe -v error -select_streams a -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 $file)
-echo $acodec
+echo "$file"
+acodec=$("$FFMPEG"ffprobe -v error -select_streams a -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$file")
+echo "$acodec"
 if [ "$acodec" = "aac" ]; then
 	newfile=${file%.*}
 	"$TERMINALNOTIFIER"terminal-notifier -title 'Convert AC3' -message "Processing $file" -activate -timeout 10
 	"$FFMPEG"ffmpeg -i "$file" -vcodec copy -scodec copy -acodec ac3 -ac 6 -ab 448k "$newfile"-AC3-.mkv
 	"$TAG"tag -a Blue "$newfile"-AC3-.mkv
-	mv $file $BACKUPDIRECTORY
+	mv "$file" "$BACKUPDIRECTORY"
 fi
 done
 
