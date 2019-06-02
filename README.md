d9db1580550489f5ffb935feac8232ea2afe097d
Steve Major <steve@themajorshome.com>
2019-06-01 20:35:13 -0400

    Remove depedency on "Trash" binary. eac3 check as well.

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 *What it does*
 
 This script will attempt to locate all mkv/m4v files in a directory, recursively looking
-in sub-directories as well. If found, it will use ffmpeg to determine if it has an aac
-audio track and attempt to re-encode the file's existing audio track to ac3. The file will
-also be converted to mkv if it was an m4v.
+in sub-directories as well. If found, it will use ffmpeg to determine if it doesn't have
+and ac3 or eac3 audio track and attempt to re-encode the file's existing audio track to
+ac3. The file will also be converted to mkv if it was an m4v.
 
 Converted files are tagged with a blue Finder label so they can be found easily.
 
 Geared towards OS X, but could easily be adapted for most *nix distros.
 
-The original file will be moved to the trash, if something goes wrong you can look there
-to reclaim the original.
+The original file will be moved to the directory of your choosing, if something goes
+wrong you can look there to reclaim the original.
 
 *Requirements*
 
@@ -20,7 +20,6 @@ to reclaim the original.
 4. Homebrew: brew install terminal-notifier
 5. Homebrew: brew install tag
 6. Homebrew: brew install detox
-7. Homebrew: brew install trash
 
 *Current Limitations*
 
--- convertac3.sh
+++ convertac3.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# Version 0.6 *See README.md for requirements*
+# Version 0.7 *See README.md for requirements*
 #
 # SET YOUR OPTIONS HERE -------------------------------------------------------------------------
 # directory to parse files recursively.
-WORKINGDIRECTORY="/Volumes/Drobo/Media Center/Unsorted-TV Shows/"
+WORKINGDIRECTORY="/Users/majorsl/Library/Containers/nz.co.pixeleyes.AutoMounter/Data/Mounts/synology/SMB/Media Center/Unsorted-TV Shows/"
 
 # path to terminal-notifier
 TERMINALNOTIFIER="/usr/local/bin/"
@@ -17,8 +17,8 @@ DETOX="/usr/local/opt/detox/bin/"
 #path to tag
 TAG="/usr/local/bin/"
 
-#path to trash (trash is a binary that trashes files using the OS X API)
-TRASH="/usr/local/bin/"
+#path to trash converted files
+TRASH="/Users/majorsl/.Trash"
 
 # -----------------------------------------------------------------------------------------------
 IFS=$'\n'
@@ -34,12 +34,12 @@ do
 echo "$file"
 acodec=$("$FFMPEG"ffprobe -v error -select_streams a -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$file")
 echo "$acodec"
-if [ "$acodec" != "ac3" ]; then
+if [[ "$acodec" != *"ac3" ]]; then
 	newfile=${file%.*}
 	"$TERMINALNOTIFIER"terminal-notifier -title 'Convert AC3' -message "Processing $file" -activate -timeout 10
 	"$FFMPEG"ffmpeg -i "$file" -vcodec copy -scodec copy -acodec ac3 -ac 6 -ab 448k "$newfile"-AC3-.mkv
 	"$TAG"tag -a Blue "$newfile"-AC3-.mkv
-	"$TRASH"trash "$file"
+	mv "$file" "$TRASH"
 fi
 done
 
