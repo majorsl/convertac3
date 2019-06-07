1799a700949aed1ae358c0a069be12c0848a8cb9
Steve Major <steve@themajorshome.com>
2019-06-07 10:22:05 -0400

    Update convertac3.sh
    Will now read current number of channels from source file and write the same number to destination. Previously, we were doing 6ch for everything.

--- convertac3.sh
+++ convertac3.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Version 0.7 *See README.md for requirements*
+# Version 0.8 *See README.md for requirements*
 #
 # SET YOUR OPTIONS HERE -------------------------------------------------------------------------
 # directory to parse files recursively.
@@ -35,9 +35,14 @@ echo "$file"
 acodec=$("$FFMPEG"ffprobe -v error -select_streams a -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$file")
 echo "$acodec"
 if [[ "$acodec" != *"ac3" ]]; then
+	achannels=$("$FFMPEG"ffprobe -v error -select_streams a -show_entries stream=channels -of default=nokey=1:noprint_wrappers=1 "$file")
+	if [ "$achannels" -gt "6" ]; then
+		achannels="6"
+	fi
+	echo "$achannels"
 	newfile=${file%.*}
 	"$TERMINALNOTIFIER"terminal-notifier -title 'Convert AC3' -message "Processing $file" -activate -timeout 10
-	"$FFMPEG"ffmpeg -i "$file" -vcodec copy -scodec copy -acodec ac3 -ac 6 -ab 448k "$newfile"-AC3-.mkv
+	"$FFMPEG"ffmpeg -i "$file" -vcodec copy -scodec copy -acodec ac3 -ac "$achannels" -ab 448k "$newfile"-AC3-.mkv
 	"$TAG"tag -a Blue "$newfile"-AC3-.mkv
 	mv "$file" "$TRASH"
 fi
