23a811090906ba10ef682c5239a4ec5184245427
Steve Major <majorsl@gmail.com>
2025-01-02 19:50:22 -0500

    Remove macOS specific items.
    Remove macOS specific items since I moved over to Linux.

--- README.md
+++ README.md
@@ -1,29 +1,15 @@
 *What it does*
 
-This script will attempt to locate all mkv/m4v files in a directory, recursively looking
-in sub-directories as well. If found, it will use ffmpeg to determine if it doesn't have
-and ac3 or eac3 audio track and attempt to re-encode the file's existing audio track to
-ac3. The file will also be converted to mkv if it was an m4v.
-
-Converted files are tagged with a blue Finder label so they can be found easily.
-
-Geared towards OS X, but could easily be adapted for most *nix distros.
+This script will locate all mkv files in a directory, recursively looking in sub-
+directories as well. If found, it will use ffmpeg to determine if it doesn't have
+an ac3 or eac3 audio track and attempt to re-encode the file's existing audio track to
+ac3. Additionally, it will remove any non-English tracks. You could modify the script for
+your preferred language.
 
 The original file will be moved to the directory of your choosing, if something goes
 wrong you can look there to reclaim the original.
 
 *Requirements*
 
-1. Working Homebrew installed.
-2. Homebrew: brew tap caskroom/cask
-3. Homebrew: brew install ffmpeg
-4. Homebrew: brew install terminal-notifier
-5. Homebrew: brew install tag
-6. Homebrew: brew install detox
-
-*Current Limitations*
-
-Files with multiple audio tracks are ignored for now. Options I'm considering 1) convert
-all if even one is acc or 2) ignoring them all if only one is an ac3 as my research
-indicates it will likely the track with the highest quality and the primary audio track.
-
+1. ffmpeg.
+2. detox
--- convertac3.sh
+++ convertac3.sh
@@ -1,51 +1,63 @@
 #!/usr/bin/env bash
-# Version 0.9 *See README.md for requirements*
+# Version 1.3 *See README.md for requirements*
 #
 # SET YOUR OPTIONS HERE -------------------------------------------------------------------------
-# directory to parse files recursively.
-WORKINGDIRECTORY="/Volumes/EG6/Downloads/Sonarr/"
-
-# path to terminal-notifier
-TERMINALNOTIFIER="/usr/local/bin/"
-
+# Default directory to parse files recursively.
+DEFAULT_WORKINGDIRECTORY="/media/majorsl/e9ef2c72-9134-4418-86dc-10742b12d0ed/Downloads/Sonarr/"
 # path to ffmpeg
-FFMPEG="/usr/local/opt/ffmpeg/bin/"
-
+FFMPEG="/usr/bin/"
 # path to detox
-DETOX="/usr/local/opt/detox/bin/"
-
-#path to tag
-TAG="/usr/local/bin/"
-
-#path to trash converted files
-TRASH="/Volumes/EG6/.Trashes/501"
-
+DETOX="/usr/bin/"
+# path to trash converted files
+TRASH="/home/majorsl/.local/share/Trash/"
 # -----------------------------------------------------------------------------------------------
 IFS=$'\n'
 
+# Check if a directory is passed as an argument
+if [ -n "$1" ]; then
+  WORKINGDIRECTORY="$1"
+else
+  WORKINGDIRECTORY="$DEFAULT_WORKINGDIRECTORY"
+fi
+
 if [ ! -d "$WORKINGDIRECTORY" ]; then
-  "$TERMINALNOTIFIER"terminal-notifier -title 'Convert AC3' -message "$WORKINGDIRECTORY doesn't exist, aborting." -activate -timeout 10
+  echo "$WORKINGDIRECTORY doesn't exist, aborting."
   exit
 fi
 
 "$DETOX"detox -r -v "$WORKINGDIRECTORY"
 for file in $(find "$WORKINGDIRECTORY" -type f -name "*.mkv")
 do
-echo "$file"
-acodec=$("$FFMPEG"ffprobe -v error -select_streams a -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$file")
-echo "$acodec"
-if [[ "$acodec" != *"ac3" ]]; then
-	achannels=$("$FFMPEG"ffprobe -v error -select_streams a -show_entries stream=channels -of default=nokey=1:noprint_wrappers=1 "$file")
-	if [ "$achannels" -gt "6" ]; then
-		achannels="6"
-	fi
-	echo "$achannels"
-	newfile=${file%.*}
-	"$TERMINALNOTIFIER"terminal-notifier -title 'Convert AC3' -message "Processing $file" -activate -timeout 10
-	"$FFMPEG"ffmpeg -i "$file" -vcodec copy -scodec copy -acodec ac3 -ac "$achannels" -ab 448k "$newfile"-AC3-.mkv
-	"$TAG"tag -a Blue "$newfile"-AC3-.mkv
-	mv "$file" $TRASH
-fi
+  echo "$file"
+  acodec=$("$FFMPEG"ffprobe -v error -select_streams a -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$file")
+  echo "$acodec"
+  
+  # Check for English audio tracks
+  eng_tracks=$("$FFMPEG"ffprobe -v error -select_streams a -show_entries stream_tags=language -of csv=p=0 "$file" | grep -n "eng" | cut -d: -f1)
+  
+  if [[ -z "$eng_tracks" ]]; then
+    echo "No English audio tracks found in $file, skipping."
+    continue
+  fi
+  
+  if [[ "$acodec" != *"ac3" ]]; then
+    achannels=$("$FFMPEG"ffprobe -v error -select_streams a -show_entries stream=channels -of default=nokey=1:noprint_wrappers=1 "$file")
+    if [ "$achannels" -gt "6" ]; then
+      achannels="6"
+    fi
+    echo "$achannels"
+    newfile=${file%.*}
+    echo "Processing $file" -activate -timeout 10
+    
+    # Create a map string for English audio tracks
+    map_str="-map 0:v -map 0:s"
+    for track in $eng_tracks; do
+      map_str="$map_str -map 0:a:$((track-1))"
+    done
+    
+    "$FFMPEG"ffmpeg -i "$file" $map_str -vcodec copy -scodec copy -acodec ac3 -ac "$achannels" -ab 448k "${newfile}-AC3-.mkv"
+    mv "$file" $TRASH
+  fi
 done
 
 unset IFS
